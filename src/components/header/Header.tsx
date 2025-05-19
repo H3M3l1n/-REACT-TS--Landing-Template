a5ae0566c218232ba0d276a57ca007757db6bf41
Andrew <hemelin23@gmail.com>
2025-05-19 23:55:41 +0300

    header update

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,34 @@
 import './header.sass';
+import logo from '../../assets/images/logo.png';
 
 const Header = () => {
   return (
-    <header id="header">
-      <h1>HEADER</h1>
+    <header id="header" className="header">
+      <img src={logo} alt="apple logo" className="header__logo" />
+      <nav className="header-nav">
+        <ul className="header-nav__list">
+          <li className="header-nav__item">
+            <a href="" className="header-nav__link">
+              Link#1
+            </a>
+          </li>
+          <li className="header-nav__item">
+            <a href="" className="header-nav__link">
+              Link#2
+            </a>
+          </li>
+          <li className="header-nav__item">
+            <a href="" className="header-nav__link">
+              Link#3
+            </a>
+          </li>
+          <li className="header-nav__item">
+            <a href="" className="header-nav__link">
+              Link#4
+            </a>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
